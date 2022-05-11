aa78ac7f83eb89531ffbcf2e3d878946ba65944c
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2022-05-11 14:30:19 +0200

    Fixed failing integration tests

--- Test/Integration/Service/StoreRestrictionValidatorTest.php
+++ Test/Integration/Service/StoreRestrictionValidatorTest.php
@@ -2,10 +2,6 @@
 
 namespace MageSuite\StoreAccessRestriction\Test\Integration\Service;
 
-/**
- * @magentoDbIsolation enabled
- * @magentoAppIsolation enabled
- */
 class StoreRestrictionValidatorTest extends \Magento\TestFramework\TestCase\AbstractController
 {
     /**
@@ -34,132 +30,105 @@ class StoreRestrictionValidatorTest extends \Magento\TestFramework\TestCase\Abst
         $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
         $this->cookieManager = $this->objectManager->create(\Magento\Framework\Stdlib\CookieManagerInterface::class);
         $this->storeManager = $this->objectManager->create(\Magento\Store\Model\StoreManagerInterface::class);
+    }
+
+    public function testItReturnsNotEmptyResponseBodyAnd200ResponseCodeForStoreViewWithDisabledAccessRestriction(): void
+    {
+        $this->dispatch('/');
+        $this->assertNotEmptyResponseBodyAnd200ResponseCode();
+    }
+
+    /**
+     * @magentoDataFixture loadRestrictedStore
+     */
+    public function testItReturnsNotEmptyResponseBodyAnd200ResponseCodeForStoreViewWithEnabledAccessRestrictionForWhitelistedIpAddress(): void
+    {
         $this->remoteAddress = $this->getMockBuilder(
             \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress::class
         )->disableOriginalConstructor()->getMock();
         $this->remoteAddress->method('getRemoteAddress')->willReturn('1.1.1.1');
         $this->objectManager->addSharedInstance($this->remoteAddress, \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress::class);
-    }
 
-    /**
-     * @magentoDbIsolation enabled
-     * @magentoAppIsolation enabled
-     */
-    public function testItReturnsNotEmptyResponseBodyAnd200ResponseCodeForStoreViewWithDisabledAccessRestriction(): void
-    {
-        $this->dispatch('http://localhost/index.php');
+        $this->dispatch('/');
         $this->assertNotEmptyResponseBodyAnd200ResponseCode();
     }
 
     /**
-     * @magentoDbIsolation enabled
-     * @magentoAppIsolation enabled
-     */
-    public function testItReturnsNotEmptyResponseBodyAnd200ResponseCodeForStoreViewWithEnabledAccessRestrictionForWhitelistedIpAddress(): void
-    {
-        $this->setAccessRestrictedAnd2IpAddressesForStoreView();
-        $this->dispatch('http://localhost/index.php');
-        $this->assertNotEmptyResponseBodyAnd200ResponseCode();
-    }
-
-    /**
-     * @magentoDbIsolation enabled
-     * @magentoAppIsolation enabled
+     * @magentoDataFixture loadRestrictedStore
      */
     public function testItReturnsEmptyResponseBodyAnd302ResponseCodeForStoreViewWithEnabledAccessRestrictionForNotWhitelistedIpAddress(): void
     {
-        $defaultStore = $this->getStoreViewWithRestrictedAccess();
-        $defaultStore->setData('allowed_ips', '3.3.3.3');
-        $this->dispatch('http://localhost/index.php');
+        $this->remoteAddress = $this->getMockBuilder(
+            \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress::class
+        )->disableOriginalConstructor()->getMock();
+        $this->remoteAddress->method('getRemoteAddress')->willReturn('3.3.3.3');
+        $this->objectManager->addSharedInstance($this->remoteAddress, \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress::class);
+
+        $this->dispatch('/');
         $this->assertEmptyResponseBodyAnd302ResponseCode();
     }
 
     /**
-     * @magentoDbIsolation enabled
-     * @magentoAppIsolation enabled
+     * @magentoDataFixture loadRestrictedStore
      */
     public function testItReturnsEmptyResponseBodyAnd302ResponseCodeForStoreViewWithEnabledAccessRestrictionAndWrongUriParam(): void
     {
-        $this->setAccessRestrictedAndCookieParamForStoreView();
-        $this->dispatch('http://localhost/index.php?bypass_store_restriction=invalid_stored_secret_value');
+        $this->dispatch('/?bypass_store_restriction=invalid_stored_secret_value');
         $this->assertEmptyResponseBodyAnd302ResponseCode();
     }
 
     /**
-     * @magentoDbIsolation enabled
-     * @magentoAppIsolation enabled
+     * @magentoDataFixture loadRestrictedStore
      */
     public function testItReturnsNotEmptyResponseBodyAnd200ResponseCodeForStoreViewWithEnabledAccessRestrictionAndValidUriParam(): void
     {
-        $this->setAccessRestrictedAndCookieParamForStoreView();
-        $this->dispatch('http://localhost/index.php?bypass_store_restriction=stored_secret_value');
+        $this->dispatch('/?bypass_store_restriction=stored_secret_value');
         $this->assertNotEmptyResponseBodyAnd200ResponseCode();
     }
 
     /**
-     * @magentoDbIsolation enabled
-     * @magentoAppIsolation enabled
+     * @magentoDataFixture loadRestrictedStore
      */
     public function testItReturnsEmptyResponseBodyAnd302ResponseCodeForStoreViewWithEnabledAccessRestrictionAndMissingCookieParam(): void
     {
-        $this->getStoreViewWithRestrictedAccess();
-        $this->dispatch('http://localhost/index.php');
+        $this->dispatch('/index.php');
         $this->assertEmptyResponseBodyAnd302ResponseCode();
     }
 
     /**
-     * @magentoDbIsolation enabled
-     * @magentoAppIsolation enabled
+     * @magentoDataFixture loadRestrictedStore
      */
     public function testItReturnsEmptyResponseBodyAnd302ResponseCodeForStoreViewWithEnabledAccessRestrictionAndWrongCookieParam(): void
     {
-        $this->setAccessRestrictedAndCookieParamForStoreView();
         $this->cookieManager->setPublicCookie(
             \MageSuite\StoreAccessRestriction\Service\StoreRestrictionValidator::RESTRICTION_BYPASS_COOKIE_NAME,
             'invalid_stored_secret_value'
         );
-        $this->dispatch('http://localhost/index.php');
+        $this->dispatch('');
         $this->assertEmptyResponseBodyAnd302ResponseCode();
     }
 
     /**
-     * @magentoDbIsolation enabled
-     * @magentoAppIsolation enabled
+     * @magentoDataFixture loadRestrictedStore
      */
     public function testItReturnsNotEmptyResponseBodyAnd200ResponseCodeForStoreViewWithEnabledAccessRestrictionAndValidCookieParam(): void
     {
-        $this->setAccessRestrictedAndCookieParamForStoreView();
         $this->cookieManager->setPublicCookie(
             \MageSuite\StoreAccessRestriction\Service\StoreRestrictionValidator::RESTRICTION_BYPASS_COOKIE_NAME,
             'stored_secret_value'
         );
-        $this->dispatch('http://localhost/index.php');
+        $this->dispatch('/');
         $this->assertNotEmptyResponseBodyAnd200ResponseCode();
     }
 
-    protected function setAccessRestrictedAnd2IpAddressesForStoreView(): void
+    public static function loadRestrictedStore()
     {
-        $defaultStore = $this->getStoreViewWithRestrictedAccess();
+        $storeManager = \Magento\Framework\App\ObjectManager::getInstance()->get(\Magento\Store\Model\StoreManager::class);
+        $defaultStore = $storeManager->getStore();
+        $defaultStore->setData('is_access_restricted', 1);
+        $defaultStore->setData('restriction_bypass_cookie_value', 'stored_secret_value');
         $defaultStore->setData('allowed_ips', '1.1.1.1,2.2.2.2');
-    }
-
-    protected function setAccessRestrictedAndCookieParamForStoreView(): void
-    {
-        $defaultStore = $this->getStoreViewWithRestrictedAccess();
-        $defaultStore->setData(
-            'restriction_bypass_cookie_value',
-            'stored_secret_value'
-        );
-    }
-
-    protected function getStoreViewWithRestrictedAccess(): \Magento\Store\Api\Data\StoreInterface
-    {
-        $defaultStore = $this->storeManager->getStore(1);
-        $defaultStore->setData(
-            'is_access_restricted',
-            1
-        );
-        return $defaultStore;
+        $defaultStore->save();
     }
 
     protected function assertNotEmptyResponseBodyAnd200ResponseCode(): void
